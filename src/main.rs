1c8fdd55eb45d4a1bdc018d564b827e816128fd7
VakeDomen <vake.domen94@gmail.com>
2025-01-13 19:52:19 +0100

    ollama request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rag::comm::ollama::OllamaClient;
+
+pub mod rag;
+
+
+
+#[tokio::main]
+async fn main() {
+    let _ = dotenv::dotenv();
+    let client = OllamaClient::default();
+    let res = client.generate("How do you code in rust?".into()).await;
+    println!("{:#?}", res);
 }
---
+++ src/rag/comm/ollama.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+use ollama_rs::{error::OllamaError, generation::completion::GenerationResponse, Ollama};
+
+use super::models::question::Questiton;
+
+
+pub struct OllamaClient {
+    ollama: Ollama,
+}
+
+impl Default for OllamaClient {
+    fn default() -> Self {
+        let ollama_host = env::var("OLLAMA_HOST").expect("OLLAMA HOST not set");
+        let ollama_port = env::var("OLLAMA_PORT").expect("OLLAMA PORT not set");
+        let ollama_port: u16 = ollama_port.parse().expect("OLLAMA_PORT not u16");
+
+        Self { 
+            ollama: Ollama::new(ollama_host, ollama_port) 
+        }
+    }
+}
+
+
+impl OllamaClient {
+    pub async fn generate(&self, question: Questiton) -> Result<GenerationResponse, OllamaError> {
+        self.ollama.generate(question.into()).await
+    }
+}
+ 
