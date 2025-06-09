ed67669fa38963deb86ec901b36df649726fb093
Mihael Berčič <mihael@regnum.si>
2025-06-10 00:57:25 +0200

    Navigation.

--- frontend/src/routes/app/+layout.svelte
+++ frontend/src/routes/app/+layout.svelte
@@ -28,36 +28,35 @@
 
 <svelte:window bind:innerWidth={width} />
 
-<div class="absolute pointer-events-none p-3 flex items-center justify-center right-0 left-0 sm:justify-start sm:px-10">
-    <img src="/woodstock.svg" class="size-14" />
-</div>
-<div class="p-2 sm:p-5 h-full bg-dark-background">
-    <div class="flex flex-col items-end sm:items-center h-full gap-5">
-        <div class="sm:relative sm:rounded-2xl sm:border sm:bg-black text-white sm:flex p-3 sm:p-3">
-            <div class="bg-black rounded-xl p-4 sm:bg-transparent sm:border-transparent sm:rounded-none sm:p-0 flex gap-5 items-center justify-end w-full">
+<div class="p-2 sm:p-5 h-full">
+    <div class="flex flex-col items-center h-full gap-5">
+        <div class="p-3 flex gap-3 items-center justify-center relative w-full">
+            <div class="p-2 bg-white backdrop-blur-2xl border-white rounded-full">
+                <img src="/woodstock.svg" class="size-8 rounded-full" />
+            </div>
+            <div class="flex items-center justify-center bg-black rounded-full p-4 sm:p-1">
                 {#if isSmall}
                     <button onclick={() => (isVisible = !isVisible)}>
                         <MaskedIcon src="../menu.svg" class="size-4 bg-white" />
                     </button>
                 {/if}
                 {#if isVisible || !isSmall}
-                    <div out:slide={{ duration: 100 }} class="z-10 select-none top-20 left-0 right-0 bg-black absolute text-white p-5 sm:relative sm:top-0 sm:translate-y-0 sm:p-0">
-                        <ul class="grid sm:flex gap-3 font-nunito" onclick={() => (isVisible = false)}>
+                    <div
+                        in:slide
+                        out:slide={{ duration: 100 }}
+                        class="z-10 absolute bottom-0 translate-y-full bg-black rounded-2xl text-white right-10 left-10 p-4
+                    sm:relative sm:rounded-full sm:py-1.5 sm:px-2 sm:flex sm:translate-y-0 sm:left-0 sm:right-0"
+                    >
+                        <ul class="grid sm:flex gap-3 sm:gap-2" onclick={() => (isVisible = false)}>
                             {#each paths as { link, text }}
                                 {@const isSelected = link == $page.url.pathname}
                                 <li>
-                                    <a
-                                        class="{text === 'Feedback' ? 'bg-orange-400' : ''} box-border border border-transparent px-5 py-1 rounded-lg transition-opacity sm:hover:bg-light-background/30 sm:hover:border-white/10 {isSelected ? 'bg-light-background/20 border border-white/10' : ''}"
-                                        href={link}
-                                    >
+                                    <a class="{text === 'Feedback' ? 'bg-orange-400' : ''} cursor-pointer flex items-center gap-3 {isSelected ? 'bg-white text-black' : ''} px-8 py-1.5 rounded-full" href={link}>
                                         {text}
-                                        {#if text === "Notifications"}
-                                            <div class="m-1 inline-block rounded-full text-xs bg-secondary/90 border-secondary border p-0.5 animate-pulse"></div>
-                                        {/if}
                                     </a>
                                 </li>
                             {/each}
-                            <div class="flex gap-3 text-red-400 group relative" onclick={logout}>
+                            <div class="flex items-center gap-3 text-red-400 group relative" onclick={logout}>
                                 <MaskedIcon src="/logout.svg" class="group-hover:bg-red-400 bg-white size-5" />
                             </div>
                         </ul>
