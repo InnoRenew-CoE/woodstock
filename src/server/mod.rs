a20fe7768f66b278b493bc4824917d8a63d1b1c1
Mihael Berčič <mihael@regnum.si>
2025-06-18 12:06:54 +0200

    Added lifetime

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -57,6 +57,7 @@ use std::io::Read;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::Mutex;
+use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio::time::sleep;
 use tokio_postgres::Client;
@@ -427,6 +428,8 @@ pub async fn start_server(rag: Rag) {
         token_signer: TokenSigner::new()
             .signing_key(secret_key.clone())
             .algorithm(Ed25519)
+            .access_token_lifetime(Duration::from_secs(86400))
+            .refresh_token_lifetime(Duration::from_secs(86400))
             .cookie_builder(Cookie::build("", "").path("/").same_site(SameSite::None))
             .build()
             .expect(""),
