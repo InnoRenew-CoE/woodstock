8fc946a95cd743937b65e12772098f3c80a7a95a
Mihael Berčič <mihael@regnum.si>
2025-09-17 11:46:07 +0200

    Adding prose-flavour

--- frontend/src/routes/app/search/+page.svelte
+++ frontend/src/routes/app/search/+page.svelte
@@ -133,7 +133,7 @@
             <div in:fade>
                 <div class="opacity-30">Woody's response</div>
                 <div class="overflow-auto p-5 flex flex-col-reverse w-full">
-                    <div class="flex-1 response preview spacing-y-2 prose w-full prose-stone">
+                    <div class="flex-1 response preview spacing-y-2 prose-sm w-full prose-stone">
                         {@html marked(data)}
                     </div>
                 </div>
--- public/_app/immutable/entry/start.D5bq5z_K.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CW_qpCzp.js";export{t as start};
