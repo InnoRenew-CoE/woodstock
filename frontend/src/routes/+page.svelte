16672cdf707d3ae1f9b398b84022ba8b9cd63b4a
Mihael Berčič <mihael@regnum.si>
2026-01-22 12:44:45 +0100

    Removing chat limitations.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,22 +3,23 @@
 
     const cards = [
         {
-            title: "Lorem Ipsum",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.",
-            image: "https://placehold.co/600x400",
-            link: "https://example.com",
+            title: "TIMBERHAUS Focus: Sustainable Wood Construction & Circular Economy",
+            description:
+                "TIMBERHAUS is a European innovation project dedicated to reducing the construction sector’s carbon footprint by developing climate-smart, circular solutions for wood construction. Addressing the fact that the building industry accounts for 40% of global CO₂ emissions and 35% of waste, TIMBERHAUS focuses on increasing the use of wood as a primary, sustainable material. The project is currently validating its technologies and strategies in three diverse pilot cities—Berlin (Germany), Baia Mare (Romania), and Siena (Italy)—to demonstrate how timber can be integrated into different urban contexts and heritage settings.",
+            image: "./timberhaus.svg",
+            link: "https://timberhaus.eu",
         },
         {
-            title: "Dolor Sit Amet",
-            description: "Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
-            image: "https://placehold.co/600x400",
-            link: "https://example.com",
-        },
-        {
-            title: "Consectetur",
+            title: "IoT Platform",
             description: "Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.",
-            image: "https://placehold.co/600x400",
-            link: "https://example.com",
+            image: "./iot.jpg",
+            link: "https://iaq.innorenew.eu/grafana/public-dashboards/535d826acc744109b4d64a68654ee262?orgId=1&refresh=10s",
+        },
+        {
+            title: "Woodstock",
+            description: "This platform.",
+            image: "./platform.jpg",
+            link: "/login",
         },
     ];
 </script>
@@ -26,7 +27,7 @@
 <div class="max-w-6xl mx-auto px-6 py-12 bg-white/60 rounded-lg text-gray-800 w-[80%] m-auto shadow-sm">
     <!-- About the Project -->
     <section class="mb-12">
-        <h1 class="text-3xl font-bold mb-4">About the Project</h1>
+        <h1 class="text-3xl font-bold mb-4">About</h1>
         <p class="text-lg leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </section>
 
@@ -34,10 +35,10 @@
     <section>
         <h2 class="text-2xl font-semibold mb-6">Visit our projects</h2>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="grid gap-10">
             {#each cards as card}
                 <div class="bg-white rounded-lg shadow-sm overflow-hidden">
-                    <img src={card.image} alt="Placeholder image" class="w-full h-40 object-cover border-2 rounded-lg shadow-xs border-white" />
+                    <img src={card.image} alt="Placeholder image" class="w-full h-[500px] object-cover border-2 rounded-lg shadow-xs border-white" />
 
                     <div class="p-4">
                         <h3 class="text-xl mb-1">
